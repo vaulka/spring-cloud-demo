8ff4ef977732ee04020460c21f059f8029b0d46d
彭森豪 <kelry@vip.qq.com>
2021-02-18 22:41:22 +0800

    fix(cloud-config-web): feign 日志打印

--- cloud-config-web/src/main/java/com/pongsky/cloud/web/aspect/RemoteCallAround.java
+++ cloud-config-web/src/main/java/com/pongsky/cloud/web/aspect/RemoteCallAround.java
@@ -9,9 +9,16 @@ import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.stereotype.Component;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.lang.reflect.Method;
+import java.util.Optional;
 
 /**
  * 远程调用耗时打印、校验远程调用响应数据
@@ -29,15 +36,31 @@ public class RemoteCallAround {
 
     @Around("execution(public * com.pongsky.cloud.feign..*.*(..))")
     public Object around(ProceedingJoinPoint joinPoint) throws Throwable {
-        ServletRequestAttributes requestAttributes
-                = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
-        if (requestAttributes == null) {
-            return null;
-        }
+        Method method = ((MethodSignature) joinPoint.getSignature()).getMethod();
         long start = System.currentTimeMillis();
         Object result = joinPoint.proceed();
         log.info("Started remote call request");
-        log.info("response is [{}]", jsonMapper.writeValueAsString(result));
+        RequestMapping request = method.getAnnotation(RequestMapping.class);
+        if (request != null) {
+            log.info("methodURL [{}] methodType [UNKNOWN]", request.value().length > 0 ? request.value()[0] : "");
+        }
+        GetMapping get = method.getAnnotation(GetMapping.class);
+        if (get != null) {
+            log.info("methodURL [{}] methodType [GET]", get.value().length > 0 ? get.value()[0] : "");
+        }
+        PutMapping put = method.getAnnotation(PutMapping.class);
+        if (put != null) {
+            log.info("methodURL [{}] methodType [PUT]", put.value().length > 0 ? put.value()[0] : "");
+        }
+        PostMapping post = method.getAnnotation(PostMapping.class);
+        if (post != null) {
+            log.info("methodURL [{}] methodType [POST]", post.value().length > 0 ? post.value()[0] : "");
+        }
+        DeleteMapping delete = method.getAnnotation(DeleteMapping.class);
+        if (delete != null) {
+            log.info("methodURL [{}] methodType [DELETE]", delete.value().length > 0 ? delete.value()[0] : "");
+        }
+        log.info("response is [{}]", jsonMapper.writeValueAsString(Optional.ofNullable(result).orElse("")));
         log.info("cost [{}] ms", System.currentTimeMillis() - start);
         log.info("Ended remote call request");
         if (result instanceof GlobalResult) {
